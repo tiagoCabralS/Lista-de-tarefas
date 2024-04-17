d9d5772bd1844f60ac4695b32ac3d239ef6c5e0e
Tiago Cabral <tiagocabral.gja@gmail.com>
2024-04-17 19:52:20 -0300

    Update __init__.py
    Termino da mudança do programa do uso de lista para dicionario.

--- lista de tarefas/funcoes/__init__.py
+++ lista de tarefas/funcoes/__init__.py
@@ -1,50 +1,60 @@
-def add(lista, prioridade=30):
+def add(dicionario, prioridade=30):
+    print('-' * 42)
     quant = int(input("Quantas tarefas deseja adicionar? "))
-
     for c in range(0, quant):
         tarefa = input(f'{c+1}° Tarefa: ')
-        lista.append([tarefa, False, prioridade])
+        # {'tarefa': ['111', '222'], 'situacao': [False, False], 'prioridade': [30, 30]}
+        dicionario['tarefa'].append(tarefa)
+        dicionario['situacao'].append(False)
+        dicionario['prioridade'].append(prioridade)
 
     print(f'\033[0;32;40mTarefas adicionadas!\033[m')
 
-def visualizar(lista):
-    for c in range(0, len(lista)):
-        print(f"\033[0;{lista[c][2]};40m[{'x' if lista[c][1] == True else ' '}] {c} - {lista[c][0]}\033[m")
+def visualizar(dicionario):
+    print('-' * 42)
+    for c in range(0, len(dicionario['tarefa'])):
+        print(f"\033[0;{dicionario['prioridade'][c]};40m[{'x' if dicionario['situacao'][c] == True else ' '}] {c} - {dicionario['tarefa'][c]}\033[m")
 
-def concluir(lista):
-    visualizar(lista)
+def concluir(dicionario):
+    print('-' * 42)
+    visualizar(dicionario)
 
     quant = int(input('Quantas tarefas deseja marcar como concluídas? '))
 
     for c in range(0, quant):
         tarefa = int(input(f'{c+1}° Tarefa: '))
-        lista[tarefa][1] = True
+        dicionario['situacao'][c] = True
 
-    print(f'\033[0;32;40mTarefas concluídas!\033[m')
+def excluir(dicionario):
+    print('-' * 42)
+    visualizar(dicionario)
+    indice = int(input("Tarefa a ser excluída: "))
+    del(dicionario['tarefa'][indice])
+    del(dicionario['situacao'][indice])
+    del(dicionario['prioridade'][indice])
 
-def excluir(lista, indice):
-    visualizar(lista)
-    lista.remove(lista[indice])
-
-def priorizar(lista):
-    visualizar(lista)
+def priorizar(dicionario):
+    print('-' * 42)
+    visualizar(dicionario)
     tarefa = int(input('Tarefa: '))
     nivel = int(input('Nível de prioridade: '))
 
     if nivel == 1:
-        lista[tarefa][2] = 32
+        dicionario['prioridade'][tarefa] = 32
     elif nivel == 2:
-        lista[tarefa][2] = 33
+        dicionario['prioridade'][tarefa] = 33
     elif nivel == 3:
-        lista[tarefa][2] = 31
+        dicionario['prioridade'][tarefa] = 31
 
-def editar(lista):
-    visualizar(lista)
+def editar(dicionario):
+    print('-' * 42)
+    visualizar(dicionario)
     tarefa = int(input('Tarefa a ser editada: '))
     nova = input("Nova tarefa: ")
-    lista[tarefa][0] = nova
+    dicionario['tarefa'][tarefa] = nova
 
 def funcoes():
+    print('-' * 42)
     print(' 1 - Visualizar lista \n',
          '2 - Adicionar tarefa \n',
          '3 - Marcar tarefa como concluída\n',
